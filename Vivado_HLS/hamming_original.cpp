b28aafa50efaf09373353ea86a8eb1142cb64614
andernil <anders.nilsen95@gmail.com>
2019-02-08 16:15:57 +0100

    Reverted to fixed array, added CMake

--- Vivado_HLS/hamming_original.cpp
+++ Vivado_HLS/hamming_original.cpp
@@ -22,26 +22,25 @@ typedef struct WAV_HEADER{
     uint16_t bitsPerSample;
     unsigned char subchunk2Id[4];
     uint32_t subchunk2Size;
-    unsigned char * data;
 } wav_hdr;
 
 void separate(complex<double>* input_data, int num_samples);
 void FFT(complex<double>* input_data, int num_samples);
-int read_wav(complex<double>* data, const char* filename);
+int read_wav(complex<double>* data_array, const char* filename);
 
 int main()
 {
-  complex<double> test_data[NUM_SAMPLES];
-  int length = read_wav(test_data, "voice_rec_live.wav");
-  cout << "First data: " << test_data[0].real() << endl;
-  int padding_needed = NUM_SAMPLES - length;
+  complex<double> wav_data[NUM_SAMPLES];
+  int wav_data_length = read_wav(wav_data, "1k_test.wav");
+  for(int i = 0; i < NUM_SAMPLES; i++)
+    //cout << wav_data[i].real() << ", ";
   return(0);
 }
 
 
 void separate (complex<double>* input_data, int num_samples)
 {
-  complex<double>* temp_buffer = new complex<double>[num_samples/2];
+  complex<double>* temp_buffer = new complex<double>[num_samples];
   //Copy all odd elements to temp buffer
   for(int i = 0; i < num_samples/2; i++)
     temp_buffer[i] = input_data[i*2+1];
@@ -77,7 +76,7 @@ void FFT(complex<double>* input_data, int num_samples)
     }
   }
 }
-int read_wav(complex<double>* data, const char* filename)
+int read_wav(complex<double>* data_array, const char* filename)
 {
   wav_hdr wavHeader;
   int headerSize = sizeof(wav_hdr);
@@ -98,14 +97,30 @@ int read_wav(complex<double>* data, const char* filename)
       cout << "Bits per sample: " << wavHeader.bitsPerSample << endl;
       long bytes = wavHeader.bitsPerSample/8;
       long buffsize= wavHeader.subchunk2Size/bytes;
-      int16_t* audiobuf = new int16_t[buffsize];
-      fread(audiobuf,bytes,buffsize,wavFile);
-      cout << "Buffsize: " << buffsize << endl
-      for(int i = 0; i < buffsize; i++){
-        data[i] = audiobuf[i];
+      if(wavHeader.bitsPerSample == 32)
+      {
+        cout << "Allocating 32-bit values" << endl;
+        int32_t* audiobuf = new int32_t[buffsize];
+        fread(audiobuf,bytes,buffsize,wavFile);
+        for(int i = 0; i < buffsize; i++)
+          data_array[i] = audiobuf[i];
+        delete[] audiobuf;
+        audiobuf = nullptr;
       }
+      else
+      {
+        cout << "Allocating 16-bit values" << endl;
+        int16_t* audiobuf = new int16_t[buffsize];
+        fread(audiobuf,bytes,buffsize,wavFile);
+        for(int i = 0; i < buffsize; i++)
+          data_array[i] = audiobuf[i];
+        delete[] audiobuf;
+        audiobuf = nullptr;
+      }
+      cout << "Buffsize: " << buffsize << endl;
       cout << "Num samples: " << buffsize << endl;
       cout << "Done reading .wav-file" << endl;
+      fclose(wavFile);
       return(buffsize);
     }
     else
